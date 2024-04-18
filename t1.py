4680a225f56d7fb95acad68ce98587d70b52aea5
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-04-18 18:28:28 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,23 +1,28 @@
 import snowflake.connector
 import pandas as pd
 from snowflake.connector import DictCursor
-
-conn = snowflake.connector.connect(
-    user="manthankumar",
-    password="Ranamkr@1201",
-    account="tq06422.ca-central-1.aws",
-    role="ACCOUNTADMIN",
-    warehouse="COMPUTE_wh",
-    database="snowlens",
-    schema="PUBLIC",
-)
-cur = conn.cursor(DictCursor)
-
-query = 'select * from "QUERY_HISTORY_TABLE"'
-cur.execute(query)
-data_fetch = cur.fetchall()
-df = pd.DataFrame(data_fetch)
-print(df)
-
-cur.close()
-conn.close()
+ 
+def fetch_data():
+    conn = snowflake.connector.connect(
+        user="manthankumar",
+        password="Ranamkr@1201",
+        account="tq06422.ca-central-1.aws",
+        role="ACCOUNTADMIN",
+        warehouse="COMPUTE_wh",
+        database="snowlens",
+        schema="PUBLIC",
+    )
+ 
+    cur = conn.cursor(DictCursor)
+    query = 'select * from "QUERY_HISTORY_TABLE"'
+    cur.execute(query)
+    data_fetch = cur.fetchall()
+    df = pd.DataFrame(data_fetch)
+    cur.close()
+    conn.close()
+    return df
+ 
+# For local debugging
+if __name__ == "__main__":
+    df = fetch_data()
+    print(df)
