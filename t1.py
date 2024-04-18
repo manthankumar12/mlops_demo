fab6350caa244fad748feca9ae8250ac26148362
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-04-18 19:00:54 +0530

    Update t1.py df1

--- t1.py
+++ t1.py
@@ -8,8 +8,9 @@ from snowflake.snowpark.functions import col
 import json
  
 def hello(session: Session) -> DataFrame:
-    df = session.sql('select * from "QUERY_HISTORY_TABLE"').show()
-    return df
+    df = session.sql('select * from "QUERY_HISTORY_TABLE"')
+    df1 = df.collect()
+    return df1
  
 # For local debugging
 if __name__ == "__main__":
