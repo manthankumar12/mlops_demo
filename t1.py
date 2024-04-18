255f4f338e3de9fbe7e8691c3b4f6a412923f522
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-04-18 18:47:51 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -8,23 +8,12 @@ from snowflake.snowpark.functions import col
 import json
  
 def hello(session: Session) -> DataFrame:
-    conn = snowflake.connector.connect(
-        user="manthankumar",
-        password="Ranamkr@1201",
-        account="tq06422.ca-central-1.aws",
-        role="ACCOUNTADMIN",
-        warehouse="COMPUTE_wh",
-        database="snowlens",
-        schema="PUBLIC",
-    )
- 
-    cur = conn.cursor(DictCursor)
+    cur = session.cursor(DictCursor)
     query = 'select * from "QUERY_HISTORY_TABLE"'
     cur.execute(query)
     data_fetch = cur.fetchall()
     df = pd.DataFrame(data_fetch)
     cur.close()
-    conn.close()
     return df
  
 # For local debugging
