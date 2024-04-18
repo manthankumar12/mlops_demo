837609cf46fb8d8d7c6ab7935222edaac7b1aefc
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-04-18 18:40:59 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,8 +1,13 @@
 import snowflake.connector
 import pandas as pd
 from snowflake.connector import DictCursor
+from snowflake.snowpark.session import Session 
+from snowflake.snowpark.session import Session 
+from snowflake.snowpark import DataFrame 
+from snowflake.snowpark.functions import col
+import json
  
-def fetch_data():
+def hello(session: Session) -> DataFrame:
     conn = snowflake.connector.connect(
         user="manthankumar",
         password="Ranamkr@1201",
@@ -24,5 +29,6 @@ def fetch_data():
  
 # For local debugging
 if __name__ == "__main__":
-    df = fetch_data()
-    print(df)
+    session = Session.builder.configs(json.load(
+      open("/change_your_path/connection.json"))).create()
+    print (hello (session).show())
