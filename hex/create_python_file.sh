f967471debbb34148f896a9873ca75a8fc4af3bc
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-04-25 18:17:34 +0530

    Delete hex/create_python_file.sh

--- hex/create_python_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-# Read the extracted source code from the temporary file
-SOURCE_CODE=$(cat extracted_code.txt)
- 
-# Write the source code to a Python file
-echo "$SOURCE_CODE" > output.py
